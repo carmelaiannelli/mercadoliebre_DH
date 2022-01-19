46cc2130abc3f186b28f446818a752c4ee35d2f8
carmelaiannelli <carmela.iannelli96@gmail.com>
2022-01-19 18:00:38 -0300

    primer commit mercadoliebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app= express();
+const path = require ('path');
+const publicPath=path.resolve(__dirname,'./public');
+
+app.use(express.static(publicPath));
+//express.static nos define a public como carpeta estatica que vamos a poder usar constantemente
+// Ahora, puede cargar los archivos que hay en el directorio public
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+
+
+app.listen(3030, () => console.log('Levantando un servidor con Express'));
+
+
